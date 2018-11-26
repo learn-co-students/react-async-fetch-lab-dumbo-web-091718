60c7273a8a67a627a202e636c0085c74cdd7900c
dianajyoo <diana.yooj@gmail.com>
2018-11-26 15:25:16 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React from 'react'
+import Greeting from './components/Greeting'
+import Button from './components/Button'
+import ExampleComponent from './components/ExampleComponent'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Greeting />
+        <ExampleComponent />
+        <Button />
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+  }
+
+}
