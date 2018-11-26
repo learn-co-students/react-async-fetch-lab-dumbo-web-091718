6e0bdf1888bc86a5f392194b19fde4d8ce917ca5
Scott Waring <scott.waring@gmail.com>
2018-11-26 16:02:31 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
-// create your App component here
+import React, {Component} from 'react';
+import Greeting from './components/Greeting'
+
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      people: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(res => res.json())
+    .then(people => {
+      this.setState({people: people.people})
+    })
+  }
+
+  render(){
+    return (
+        <div>
+        <Greeting people={this.state.people}/>
+        </div>
+    )
+  }
+}
+
+export default App;
--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 
 class Greeting extends Component {
 
+  mapped =(person)=> {
+      return (
+        <div>
+        <p>Hello! {person.name}</p>
+        <p> Their Craft: {person.craft}</p>
+        </div>
+      );
+  }
+
+
   render() {
-    return (
-      <div className="Greeting">
-        Hello!
+    return(
+      <div>
+        {this.props.people.map(person => this.mapped(person))}
       </div>
-    );
+    )
   }
 
 }
